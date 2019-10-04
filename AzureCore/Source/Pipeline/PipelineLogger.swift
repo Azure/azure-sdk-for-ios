1e62a37ab1b0888f8a0e0ddd931fd1f9ff6afd93
Brandon Siegel <brandon.siegel@microsoft.com>
2019-10-04 14:01:12 -0700

    Add logging-related source files

---
+++ AzureCore/Source/Pipeline/PipelineLogger.swift
@@ -0,0 +1,9 @@
+//
+//  PipelineLogger.swift
+//  AzureCore
+//
+//  Created by Brandon Siegel on 10/3/19.
+//  Copyright © 2019 Azure SDK Team. All rights reserved.
+//
+
+import Foundation
---
+++ AzureCore/Source/Pipeline/Policies/LoggingPolicy.swift
@@ -0,0 +1,9 @@
+//
+//  LoggingPolicy.swift
+//  AzureCore
+//
+//  Created by Brandon Siegel on 10/2/19.
+//  Copyright © 2019 Azure SDK Team. All rights reserved.
+//
+
+import Foundation
