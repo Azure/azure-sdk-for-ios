5adcc1c9f4d43290a99bfda1655bff6a2b56e93c
Brandon Siegel <brandon.siegel@microsoft.com>
2020-02-11 11:29:27 -0800

    Specify path of config files in swiftformat

--- Scripts/swiftlint.sh
+++ Scripts/swiftlint.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 #  swiftlint.sh
+REPO_ROOT=$(dirname "$0")/..
 
 if which swiftformat >/dev/null; then
     echo "Formatting Swift files at paths $@"
-    swiftformat --quiet $@
+    swiftformat --quiet --config "$REPO_ROOT/.swiftformat" $@
 else
     echo "warning: SwiftFormat not installed. Download from https://github.com/nicklockwood/SwiftFormat"
 fi
 
 if which swiftlint >/dev/null; then
     echo "Correcting Swift files at paths $@"
-    swiftlint autocorrect --quiet --path $@
+    swiftlint autocorrect --quiet --config "$REPO_ROOT/.swiftlint.yml" --path $@
     echo "Linting Swift files at paths $@"
-    swiftlint lint --quiet --path $@
+    swiftlint lint --quiet --config "$REPO_ROOT/.swiftlint.yml" --path $@
 else
     echo "warning: SwiftLint not installed. Download from https://github.com/realm/SwiftLint"
 fi
