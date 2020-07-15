f9b80a02d40bd68989c1bc1a21730b4cf37ce5e2
Brandon Siegel <brandon.siegel@microsoft.com>
2020-07-14 19:20:46 -0700

    Move restorationId into ClientOptions

--- sdk/storage/AzureStorageBlob/Source/Options/ClientOptions.swift
+++ sdk/storage/AzureStorageBlob/Source/Options/ClientOptions.swift
@@ -38,20 +38,34 @@ public struct StorageBlobClientOptions: AzureConfigurable {
 
     // Blob operations
 
+    /// An identifier used to associate this client with transfers it creates. When a transfer is reloaded from disk
+    /// (e.g. after an application crash), it can only be resumed once a client with the same `restorationId` has been
+    /// initialized. If your application only uses a single `StorageBlobClient`, it is recommended to use a value unique
+    /// to your application (e.g. "MyApplication"). If your application uses multiple clients with different
+    /// configurations, use a value unique to both your application and the configuration (e.g.
+    /// "MyApplication.userClient").
+    public let restorationId: String
+
+    /// The maximum size of a single chunk in a blob upload or download.
+    public let maxChunkSizeInBytes: Int
+
     /// The `TransferNetworkPolicy` to use for managed downloads.
     public let downloadNetworkPolicy: TransferNetworkPolicy
 
     /// The `TransferNetworkPolicy` to use for managed uploads.
     public let uploadNetworkPolicy: TransferNetworkPolicy
 
-    /// The maximum size of a single chunk in a blob upload or download.
-    public let maxChunkSizeInBytes: Int
-
     /// Initialize a `StorageBlobClientOptions` structure.
     /// - Parameters:
     ///   - apiVersion: The API version of the Azure Storage Blob service to invoke.
     ///   - logger: The `ClientLogger` to be used by this `StorageBlobClient`.
     ///   - telemetryOptions: Options for configuring telemetry sent by this `StorageBlobClient`.
+    ///   - restorationId: An identifier used to associate this client with transfers it creates. When a transfer is
+    ///     reloaded from disk (e.g. after an application crash), it can only be resumed once a client with the same
+    ///     `restorationId` has been initialized. If your application only uses a single `StorageBlobClient`, it is
+    ///     recommended to use a value unique to your application (e.g. "MyApplication"). If your application uses
+    ///     multiple clients with different configurations, use a value unique to both your application and the
+    ///     configuration (e.g. "MyApplication.userClient").
     ///   - maxChunkSizeInBytes: The maximum size of a single chunk in a blob upload or download.
     ///     Must be less than 4MB if enabling MD5 or CRC64 hashing.
     ///   - downloadNetworkPolicy: The `TransferNetworkPolicy` to use for managed downloads.
@@ -60,6 +74,7 @@ public struct StorageBlobClientOptions: AzureConfigurable {
         apiVersion: StorageBlobClient.ApiVersion = .latest,
         logger: ClientLogger = ClientLoggers.default(tag: "StorageBlobClient"),
         telemetryOptions: TelemetryOptions = TelemetryOptions(),
+        restorationId: String = DeviceProviders.appBundleInfo.identifier ?? "AzureStorageBlob",
         maxChunkSizeInBytes: Int = 4 * 1024 * 1024 - 1,
         downloadNetworkPolicy: TransferNetworkPolicy? = nil,
         uploadNetworkPolicy: TransferNetworkPolicy? = nil
@@ -68,6 +83,7 @@ public struct StorageBlobClientOptions: AzureConfigurable {
         self.logger = logger
         self.telemetryOptions = telemetryOptions
         self.maxChunkSizeInBytes = maxChunkSizeInBytes
+        self.restorationId = restorationId
         self.downloadNetworkPolicy = downloadNetworkPolicy ?? TransferNetworkPolicy.default
         self.uploadNetworkPolicy = uploadNetworkPolicy ?? TransferNetworkPolicy.default
     }
