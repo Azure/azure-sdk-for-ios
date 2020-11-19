3ad0099925b644562c4922199bda2b2872e790c0
JoshuaLai <9044372+JoshuaLai@users.noreply.github.com>
2020-11-19 17:37:06 -0500

    [CommunciationUserCredentialTests] Adding new obj-c unit tests (#503)
    * [CommunciationUserCredentialTests] Added new objective-c unit tests to test implementation
    
    * [CommunciationUserCredentialTests] Prepending the unit tests with ObjC
    
    * [CommunciationUserCredentialTests] Cleaning up the token expiring unit tests
    
    * [ObjCCommunciationUserCredentialTests] Rename the ObjC test file so it is called out
    
    * [ObjCCommunciationUserCredentialTests] Updating unit tests to use the new CommunicationAccessToken instead of AccessToken
    
    * [CI] updating the simulator and xcode version that ci will run
    
    * Preserve test logs on failure
    
    * Turn off test parallelization for Communication
    
    * Erase simulators before running tests
    
    * [ObjCCommunciationUserCredentialTests] Remove the dispathc waits in case it is breaking CI
    
    * [CI] Adding a new command to boot sim and wait a bit before next task
    
    * [ObjCCommunciationUserCredentialTests] Temp remove all tests but one simple one.
    
    * [ObjCCommunciationUserCredentialTests] adding back in the decode token test
    
    * [ObjCCommunciationUserCredentialTests] ObjCRefreshTokenProactively_TokenAlreadyExpired bring back this unit test
    
    * [ObjCCommunciationUserCredentialTests] ObjCRefreshTokenProactively_FetchTokenReturnsError adding in the unit test
    
    * [ObjCCommunciationUserCredentialTests] change the expectation style
    
    * Update all.xcscheme
    
    * [ObjCCommunciationUserCredentialTests] ObjCRefreshTokenOnDemand_AsyncRefresh adding back in unit test
    
    * [ObjCCommunicationUserCredentialAsyncTests] Split up the tests into different classes
    
    * [ObjCCommunicationUserCredentialAsyncTests] moving the timing test to async
    
    * Update all.xcscheme
    
    Undo the turning off parallel runs
    
    * [ObjCCommunicationUserCredentialAsyncTests] Adding to statuic properties to clean up tests as per feedback
    
    * [ObjCCommunicationUserCredentialAsyncTests] Removing redundant test
    
    * [ObjCCommunciationUserCredentialTests] Clean up the invalid unit tests
    
    * [ObjCCommunciationUserCredentialTests] adding in new checks to insure error not nil for malformat token
    
    * [ObjCCommunciationUserCredentialTests] adding in new checks to insure error not nil for malformat token
    
    * [ObjCTokenParserTests] Splitting up responsibility of tests
    
    * [ObjCCommunciationUserCredentialTests] Disable a decode test
    
    * Update archetype-sdk-client.yml
    
    Remove publish of the artifacts
    
    * Remove the sim erase and shutdown
    
    * [ObjCCommunicationUserCredentialAsyncTests] Fixing the test removing unused properties
    
    Co-authored-by: Brandon Siegel <brsiegel@microsoft.com>

---
+++ sdk/communication/AzureCommunication/Tests/ObjCCommunciationUserCredentialTests.m
@@ -0,0 +1,128 @@
+// --------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// The MIT License (MIT)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the ""Software""), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+// --------------------------------------------------------------------------
+
+#import <XCTest/XCTest.h>
+#import <AzureCommunication/AzureCommunication-Swift.h>
+#import <AzureCore/AzureCore-Swift.h>
+
+@interface ObjCCommunciationUserCredentialTests : XCTestCase
+@property (nonatomic, strong) NSString *sampleToken;
+@property (nonatomic, strong) NSString *sampleExpiredToken;
+@property (nonatomic) double sampleTokenExpiry;
+@property (nonatomic) int fetchTokenCallCount;
+@end
+
+@implementation ObjCCommunciationUserCredentialTests
+
+- (void)setUp {
+    [super setUp];
+    
+    self.sampleToken = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjMyNTAzNjgwMDAwfQ.9i7FNNHHJT8cOzo-yrAUJyBSfJ-tPPk2emcHavOEpWc";
+    self.sampleExpiredToken = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjEwMH0.1h_scYkNp-G98-O4cW6KvfJZwiz54uJMyeDACE4nypg";
+    self.sampleTokenExpiry = 32503680000;
+    self.fetchTokenCallCount = 0;
+}
+
+- (void)xtest_ObjCDecodeToken {
+    XCTestExpectation *expectation = [self expectationWithDescription:
+                                      @"DecodeToken"];
+
+    CommunicationUserCredential *userCredential = [[CommunicationUserCredential alloc] initWithToken: self.sampleToken
+                                                                                               error: nil];
+    
+    [userCredential tokenWithCompletionHandler:^(CommunicationAccessToken *accessToken, NSError * error) {
+        XCTAssertNil(error);
+        XCTAssertEqual(accessToken.token, self.sampleToken);
+        XCTAssertEqual(accessToken.expiresOn.timeIntervalSince1970, self.sampleTokenExpiry);
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectations:@[expectation] timeout:2.0];
+}
+
+- (void)test_ObjCRefreshTokenProactively_TokenAlreadyExpired {
+    XCTestExpectation *expectation = [self expectationWithDescription:
+                                      @"RefreshTokenProactively_TokenAlreadyExpired"];
+    __weak ObjCCommunciationUserCredentialTests *weakSelf = self;
+    
+    CommunicationUserCredential *credential = [[CommunicationUserCredential alloc]
+                                               initWithInitialToken:self.sampleExpiredToken
+                                               refreshProactively:YES
+                                               error:nil
+                                               tokenRefresher:^(void (^ block)
+                                                                (NSString * _Nullable accessToken,
+                                                                 NSError * _Nullable error)) {
+        weakSelf.fetchTokenCallCount += 1;
+        block(weakSelf.sampleToken, nil);
+    }];
+    
+    [credential tokenWithCompletionHandler:^(CommunicationAccessToken * _Nullable accessToken,
+                                             NSError * _Nullable error) {
+        XCTAssertNotNil(accessToken);
+        XCTAssertNil(error);
+        XCTAssertEqual(accessToken.token, weakSelf.sampleToken);
+        XCTAssertEqual(weakSelf.fetchTokenCallCount, 1);
+        
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectations:@[expectation] timeout:2.0];
+}
+
+- (void)test_ObjCRefreshTokenProactively_FetchTokenReturnsError {
+    XCTestExpectation *expectation = [self expectationWithDescription:
+                                      @"RefreshTokenProactively_FetchTokenReturnsError"];
+    __weak ObjCCommunciationUserCredentialTests *weakSelf = self;
+    NSString *errorDesc = @"Error while fetching token";
+    CommunicationUserCredential *credential = [[CommunicationUserCredential alloc]
+                                               initWithInitialToken:self.sampleExpiredToken
+                                               refreshProactively:YES
+                                               error:nil
+                                               tokenRefresher:^(void (^ block)
+                                                                (NSString * _Nullable token,
+                                                                 NSError * _Nullable error)) {
+        weakSelf.fetchTokenCallCount += 1;
+        
+        NSDictionary *errorDictionary = @{ NSLocalizedDescriptionKey: errorDesc};
+        NSError *error = [[NSError alloc] initWithDomain:NSOSStatusErrorDomain code:400 userInfo:errorDictionary];
+        
+        block(nil, error);
+    }];
+    
+    [credential tokenWithCompletionHandler:^(CommunicationAccessToken * _Nullable accessToken,
+                                             NSError * _Nullable error) {
+        XCTAssertNotNil(error);
+        XCTAssertEqual([error.localizedDescription containsString: errorDesc], YES);
+        XCTAssertNil(accessToken);
+        XCTAssertEqual(weakSelf.fetchTokenCallCount, 1);
+        
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectations:@[expectation] timeout:2.0];
+}
+
+@end
