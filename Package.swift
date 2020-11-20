b4693316a1b5bb3dd2b9a5c3fa4ae197fe34bfaf
Josh Lai <joshlai@microsoft.com>
2020-11-20 15:53:52 -0800

    [Package] Testing the removing files from exception should throw a CI error

--- Package.swift
+++ Package.swift
@@ -72,8 +72,6 @@ let package = Package(
             dependencies: ["AzureCommunication"],
             path: "sdk/communication/AzureCommunication",
             exclude: [
-                "Tests/AzureCommunicationTests-Bridging-Header.h",
-                "Tests/ObjCCommunciationUserCredentialTests.m",
                 "Tests/ObjCCommunicationUserCredentialAsyncTests.m",
                 "Tests/ObjCTokenParserTests.m"
             ],
