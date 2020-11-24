d1c58233420897a29894039c536aa6eb3a392f25
knvsl <10719831+knvsl@users.noreply.github.com>
2020-11-24 11:52:47 -0800

    Regenerate Chat SDK (#574)
    * [Communication] - [Package] - Exclude ObjC files  (#572)
    
    * [Package] Exclude the obj-c in package, and include swift build command in CI script
    
    * [Package] Testing the removing files from exception should throw a CI error
    
    * [Package] bring back the excluded files for CI
    
    * [CI] making swift build its own job
    
    * fixup! [CI] making swift build its own job
    
    Co-authored-by: Brandon Siegel <brsiegel@microsoft.com>
    
    * Regenerate chat
    
    * Remove the old stuff
    
    * Manual fixes
    
    * Fix types
    
    * More type updates
    
    * Remove MergePatchObject
    
    * Update names, remove Custom folder
    
    * More rename member to participant
    
    * Remove comment
    
    * Regenerate again with latest autorest fixes
    
    * Revert readme
    
    * Typo again
    
    * Lint
    
    Co-authored-by: JoshuaLai <9044372+JoshuaLai@users.noreply.github.com>
    Co-authored-by: Brandon Siegel <brsiegel@microsoft.com>

--- sdk/communication/AzureCommunicationChat/Source/Generated/AzureCommunicationChatClient.swift
+++ sdk/communication/AzureCommunicationChat/Source/Generated/AzureCommunicationChatClient.swift
@@ -23,12 +23,12 @@ public final class AzureCommunicationChatClient: PipelineClient, PageableClient 
 
     /// API version of the  to invoke. Defaults to the latest.
     public enum ApiVersion: String {
-        /// API version "2020-09-21-preview2"
-        case v20200921preview2 = "2020-09-21-preview2"
+        /// API version "2020-11-01-preview3"
+        case v20201101preview3 = "2020-11-01-preview3"
 
         /// The most recent API version of the
         public static var latest: ApiVersion {
-            return .v20200921preview2
+            return .v20201101preview3
         }
     }
 
