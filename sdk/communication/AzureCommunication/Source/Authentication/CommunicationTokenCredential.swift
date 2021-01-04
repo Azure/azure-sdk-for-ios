4e569d13617d896fcaf437b3e821472a805973f1
Josh Lai <joshlai@microsoft.com>
2021-01-04 15:32:50 -0500

    [CommunicationTokenCredential] removing extra space

--- sdk/communication/AzureCommunication/Source/Authentication/CommunicationTokenCredential.swift
+++ sdk/communication/AzureCommunication/Source/Authentication/CommunicationTokenCredential.swift
@@ -78,7 +78,7 @@ public typealias TokenRefreshOnCompletion = (String?, Error?) -> Void
     /**
      Retrieve an access token from the credential.
      - Parameter completionHandler: Closure that accepts an optional `AccessToken` or optional `Error` as parameters.
-     `AccessToken` returns  a token and an expiry date if applicable. `Error` returns `nil` if the current token can be returned.
+     `AccessToken` returns a token and an expiry date if applicable. `Error` returns `nil` if the current token can be returned.
      */
     public func token(completionHandler: @escaping CommunicationTokenCompletionHandler) {
         userTokenCredential.token(completionHandler: completionHandler)
