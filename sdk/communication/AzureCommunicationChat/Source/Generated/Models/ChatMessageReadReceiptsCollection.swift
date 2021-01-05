ba9e0a5aa7696ce86017ff5aa0fa34f4f5f52c70
knvsl <10719831+knvsl@users.noreply.github.com>
2021-01-04 17:18:50 -0800

    Regenerate Chat

--- sdk/communication/AzureCommunicationChat/Source/Generated/Models/ChatMessageReadReceiptsCollection.swift
+++ sdk/communication/AzureCommunicationChat/Source/Generated/Models/ChatMessageReadReceiptsCollection.swift
@@ -40,8 +40,8 @@ public struct ChatMessageReadReceiptsCollection: Codable {
     // MARK: Codable
 
     enum CodingKeys: String, CodingKey {
-        case value
-        case nextLink
+        case value = "value"
+        case nextLink = "nextLink"
     }
 
     /// Initialize a `ChatMessageReadReceiptsCollection` structure from decoder
--- sdk/communication/AzureCommunicationChat/Source/Generated/Models/Enumerations.swift
+++ sdk/communication/AzureCommunicationChat/Source/Generated/Models/Enumerations.swift
@@ -13,7 +13,7 @@ import Foundation
 
 /// The chat message priority.
 public enum ChatMessagePriority: RequestStringConvertible, Codable, Equatable {
-    /// Custom value for unrecognized enum values
+    // Custom value for unrecognized enum values
     case custom(String)
 
     case normal
@@ -25,9 +25,9 @@ public enum ChatMessagePriority: RequestStringConvertible, Codable, Equatable {
         case let .custom(val):
             return val
         case .normal:
-            return "Normal"
+            return "normal"
         case .high:
-            return "High"
+            return "high"
         }
     }
 
@@ -37,6 +37,69 @@ public enum ChatMessagePriority: RequestStringConvertible, Codable, Equatable {
             self = .normal
         case "high":
             self = .high
+        default:
+            self = .custom(val)
+        }
+    }
+
+    // MARK: Codable
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let value = try container.decode(String.self)
+        self.init(value)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(requestString)
+    }
+}
+
+/// The chat message type.
+public enum ChatMessageType: RequestStringConvertible, Codable, Equatable {
+    // Custom value for unrecognized enum values
+    case custom(String)
+
+    case text
+
+    case html
+
+    case topicUpdated
+
+    case participantAdded
+
+    case participantRemoved
+
+    public var requestString: String {
+        switch self {
+        case let .custom(val):
+            return val
+        case .text:
+            return "text"
+        case .html:
+            return "html"
+        case .topicUpdated:
+            return "topicUpdated"
+        case .participantAdded:
+            return "participantAdded"
+        case .participantRemoved:
+            return "participantRemoved"
+        }
+    }
+
+    public init(_ val: String) {
+        switch val.lowercased() {
+        case "text":
+            self = .text
+        case "html":
+            self = .html
+        case "topicupdated":
+            self = .topicUpdated
+        case "participantadded":
+            self = .participantAdded
+        case "participantremoved":
+            self = .participantRemoved
         default:
             self = .custom(val)
         }
