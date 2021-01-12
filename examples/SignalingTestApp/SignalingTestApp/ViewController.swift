44310f8deac0d63436736a590f89bf8e02666ab7
glorialimicrosoft <gelli@microsoft.com>
2021-01-11 16:00:58 -0800

    add signaling test app
    add signaling test app

---
+++ examples/SignalingTestApp/SignalingTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignalingTestApp
+//
+//  Created by Gloria Li on 2021-01-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
