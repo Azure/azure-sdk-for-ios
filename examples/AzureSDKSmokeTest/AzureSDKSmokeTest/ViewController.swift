02efae72432cfe35096f9fc953a1d3f57301a4d1
Travis Prescott <tjprescott@users.noreply.github.com>
2021-01-15 13:51:54 -0800

    Add smoke test project (#639)
    * Smoke test example.
    
    * Smoke test
    
    * Commit working podfile.
    
    * Clean up schemes.
    
    * Add support to test static and dynamic frameworks.
    
    * Remove unused test target.

--- examples/AzureSDKSmokeTest/AzureSDKSmokeTest/ViewController.swift
+++ examples/AzureSDKSmokeTest/AzureSDKSmokeTest/ViewController.swift
@@ -24,27 +24,15 @@
 //
 // --------------------------------------------------------------------------
 
-@testable import AzureSDKDemoSwift
-import XCTest
+import UIKit
 
-class AzureSDKDemoSwiftTests: XCTestCase {
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 }
+
