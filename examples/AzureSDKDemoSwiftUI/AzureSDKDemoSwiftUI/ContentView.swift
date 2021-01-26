bdf99a1bf801a97215e196a17766f31b49eb42e9
Josh Lai <joshlai@microsoft.com>
2021-01-26 18:17:02 -0500

    [Content] breaking up the view into different structs

--- examples/AzureSDKDemoSwiftUI/AzureSDKDemoSwiftUI/ContentView.swift
+++ examples/AzureSDKDemoSwiftUI/AzureSDKDemoSwiftUI/ContentView.swift
@@ -1,17 +1,42 @@
+// --------------------------------------------------------------------------
 //
-//  ContentView.swift
-//  AzureSDKDemoSwiftUI
+// Copyright (c) Microsoft Corporation. All rights reserved.
 //
-//  Created by Joshua Lai on 2021-01-25.
+// The MIT License (MIT)
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the ""Software""), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+// --------------------------------------------------------------------------
 
 import SwiftUI
+
+import AzureCore
+import MSAL
 
 struct ContentView: View {
     @State private var isLoggedIn = false
     @State private var isShowingMedia = false
     
+    @State private var isError = false
+    
     @State private var userLabel: String = "Please log in"
+    @State private var errorMessage: String = ""
     
     var body: some View {
         NavigationView {
@@ -22,12 +47,28 @@ struct ContentView: View {
             }.navigationBarItems(
                 leading:
                     Button("Log Out") {
+                        guard let application = AppState.application else { return }
+                        guard let account = AppState.currentAccount else { return }
                         
+                        do {
+                            try application.remove(account)
+                            AppState.account = nil
+                            isLoggedIn = false
+                        } catch {
+                            // show an alert
+                            errorMessage = parseErrorMessage(using: error)
+                            isError = true
+                        }
                     }.font(.title3)
-                    .disabled(!isLoggedIn),
+                    .disabled(!isLoggedIn)
+                    .alert(isPresented: $isError) {
+                        Alert(title: Text("Error!"),
+                              message: Text(errorMessage),
+                              dismissButton: .cancel())
+                    },
                 trailing:
                     NavigationLink(
-                        destination: MediaViewController(),
+                        destination: MediaView(),
                         isActive: $isShowingMedia,
                         label: {
                             Button("Media") {
@@ -37,28 +78,22 @@ struct ContentView: View {
             )
         }
     }
-}
-
-struct MediaViewController: View {
-    @State var selectedTabItem: Int = 0
-    @Environment(\.presentationMode) var presentationMode
     
-    var body: some View {
-        TabView(selection: $selectedTabItem,
-                content:  {
-                    Text("Tab Content 1")
-                        .tabItem {
-                            Image(systemName: "square.and.arrow.down")
-                            Text("Download")
-                        }
-                        .tag(0)
-                    Text("Tab Content 2")
-                        .tabItem {
-                            Image(systemName: "square.and.arrow.up")
-                            Text("Upload")
-                        }
-                        .tag(1)
-                })
+    private func parseErrorMessage(using error: Error?) -> String {
+        var errorString: String = ""
+        if let err = error {
+            switch error {
+            case let azureError as AzureError:
+                errorString = azureError.message
+            default:
+                let errorInfo = (err as NSError).userInfo
+                errorString = errorInfo[NSDebugDescriptionErrorKey] as? String ?? err.localizedDescription
+            }
+        } else {
+            errorString = "An error occurred."
+        }
+        
+        return errorString
     }
 }
 
--- examples/AzureSDKDemoSwiftUI/AzureSDKDemoSwiftUI/Utils/Common.swift
+++ examples/AzureSDKDemoSwiftUI/AzureSDKDemoSwiftUI/Utils/Common.swift
@@ -24,4 +24,166 @@
 //
 // --------------------------------------------------------------------------
 
+import AzureCore
+import AzureIdentity
+import AzureStorageBlob
 import Foundation
+import MSAL
+
+struct AppConstants {
+    static let storageAccountUrl = URL(string: "https://iosdemostorage1.blob.core.windows.net/")!
+
+    static let tenant = "7e6c9611-413e-47e4-a054-a389854dd732"
+
+    static let clientId = "6f2c62dd-d6b2-444a-8dff-c64380e7ac76"
+
+    static let redirectUri = "msauth.com.azure.examples.AzureSDKDemoSwift://auth"
+
+    static let authority = "https://login.microsoftonline.com/7e6c9611-413e-47e4-a054-a389854dd732"
+
+    static let uploadContainer: String! = "uploads"
+
+    static let videoContainer: String! = "videos"
+}
+
+struct AppState {
+    static var application: MSALPublicClientApplication?
+
+    static var account: MSALAccount?
+
+    static let scopes = [
+        "https://storage.azure.com/.default"
+    ]
+
+    static var currentAccount: MSALAccount? {
+        if let account = AppState.account {
+            return account
+        }
+        guard let application = AppState.application else { return nil }
+        // We retrieve our current account by getting the first account from cache
+        // In multi-account applications, account should be retrieved by home account identifier or username instead
+        do {
+            let cachedAccounts = try application.allAccounts()
+
+            if !cachedAccounts.isEmpty {
+                return cachedAccounts.first
+            }
+        } catch let error as NSError {
+            print("Didn't find any accounts in cache: \(error)")
+        }
+        return nil
+    }
+
+    static var uploadOptions: UploadBlobOptions {
+        return UploadBlobOptions()
+    }
+
+    static var downloadOptions: DownloadBlobOptions {
+        let options = DownloadBlobOptions(
+            range: RangeOptions(calculateMD5: true)
+        )
+        return options
+    }
+
+    private static var internalBlobClient: StorageBlobClient?
+    static func blobClient(withDelegate delegate: StorageBlobClientDelegate? = nil) throws -> StorageBlobClient {
+        if AppState.internalBlobClient == nil {
+            guard let application = AppState.application else {
+                fatalError("Application is not initialized. Unable to create Blob Storage Client.")
+            }
+            let credential = MSALCredential(
+                tenant: AppConstants.tenant, clientId: AppConstants.clientId, application: application,
+                account: AppState.currentAccount
+            )
+            let options = StorageBlobClientOptions(
+                logger: ClientLoggers.none,
+                transportOptions: TransportOptions(timeout: 5.0),
+                restorationId: "AzureSDKDemoSwift"
+            )
+            AppState.internalBlobClient = try? StorageBlobClient(
+                endpoint: AppConstants.storageAccountUrl,
+                credential: credential,
+                withOptions: options
+            )
+        }
+
+        let client = AppState.internalBlobClient!
+        client.delegate = delegate
+        StorageBlobClient.maxConcurrentTransfers = 4
+        return client
+    }
+}
+
+class ActivtyViewController: UIViewController {
+    internal var spinner = UIActivityIndicatorView(style: .medium)
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.7)
+
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        view.addSubview(spinner)
+
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
+
+extension UIViewController {
+    internal func showAlert(error: Error?) {
+        guard presentedViewController == nil else { return }
+        var errorString: String
+        if let err = error {
+            switch error {
+            case let azureError as AzureError:
+                errorString = azureError.message
+            default:
+                let errorInfo = (err as NSError).userInfo
+                errorString = errorInfo[NSDebugDescriptionErrorKey] as? String ?? err.localizedDescription
+            }
+        } else {
+            errorString = "An error occurred."
+        }
+        let alertController = UIAlertController(title: "Error!", message: errorString, preferredStyle: .alert)
+        let title = NSAttributedString(string: "Error!", attributes: [
+            NSAttributedString.Key.foregroundColor: UIColor.red
+        ])
+        alertController.setValue(title, forKey: "attributedTitle")
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true)
+    }
+
+    internal func showAlert(message: String) {
+        guard presentedViewController == nil else { return }
+        let alertController = UIAlertController(title: "Blob Contents", message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "Close", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true)
+    }
+}
+
+extension TransferState {
+    public var color: UIColor {
+        switch self {
+        case .pending:
+            return UIColor(red: 222.0 / 255.0, green: 222.0 / 255.0, blue: 222.0 / 255.0, alpha: 1.0)
+        case .inProgress:
+            return UIColor(red: 128.0 / 255.0, green: 230.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+        case .paused:
+            return UIColor(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 128.0 / 255.0, alpha: 1.0)
+        case .complete:
+            return UIColor(red: 128.0 / 255.0, green: 255.0 / 255.0, blue: 128.0 / 255.0, alpha: 1.0)
+        case .failed:
+            return .systemRed
+        case .canceled:
+            return .systemRed
+        case .deleted:
+            return .systemRed
+        @unknown default:
+            return .white
+        }
+    }
+}
+
