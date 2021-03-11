599b921b90db04d608051c0aa2a9fdddd5233af5
Travis Prescott <trpresco@microsoft.com>
2021-03-11 12:12:05 -0800

    Update Package.swift.

--- Package.swift
+++ Package.swift
@@ -38,6 +38,7 @@ let package = Package(
         .library(name: "AzureCore", targets: ["AzureCore"]),
         .library(name: "AzureCommunication", targets: ["AzureCommunication"]),
         .library(name: "AzureCommunicationChat", targets: ["AzureCommunicationChat"]),
+        .library(name: "AzureCommunicationCalling", targets: ["AzureCommunicationCalling"]),
     ],
     dependencies: [
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.1.0")
@@ -77,6 +78,11 @@ let package = Package(
                 "Package.swift"
             ],
             sources: ["Source"]
+        ),
+        .binaryTarget(
+            name: "AzureCommunicationCalling",
+            url: "https://github.com/Azure/Communication/releases/download/v1.0.0-beta.9/AzureCommunicationCalling-1.0.0-beta9.zip",
+            checksum: "73c56437ee21ff4c5a868e88e6351ede32445b21f0d9d9f8d31bbb739e4f57a1"
         ),
         // Test targets
         .testTarget(
