e4858772cd1130c8b012cb61ffb61c52927ecc3b
Travis Prescott <trpresco@microsoft.com>
2021-03-24 10:19:51 -0700

    Remove Trouter references.

--- Package.swift
+++ Package.swift
@@ -39,7 +39,6 @@ let package = Package(
         .library(name: "AzureCommunication", targets: ["AzureCommunication"]),
         .library(name: "AzureCommunicationChat", targets: ["AzureCommunicationChat"]),
         .library(name: "AzureCommunicationCalling", targets: ["AzureCommunicationCalling"]),
-        .library(name: "TrouterClientIos", targets: ["TrouterClientIos"]),
     ],
     dependencies: [
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.1.0")
@@ -84,11 +83,6 @@ let package = Package(
             name: "AzureCommunicationCalling",
             url: "https://github.com/Azure/Communication/releases/download/v1.0.0-beta.9/AzureCommunicationCalling-1.0.0-beta9.zip",
             checksum: "b632ae508ac5f271ef7b5d83ee2476dc8175b12341e0a32aa079cce87bc19577"
-        ),
-        .binaryTarget(
-          name: "TrouterClientIos",
-          url: "https://github.com/microsoft/trouter-client-ios/releases/download/v0.0.1-beta.1/TrouterClientIos-0.0.1-beta.1.zip",
-          checksum: "bd568e938c51b6c91a4557b04a99a9750f5503c33fc24a7f4e82834fd2452e87"
         ),
         // Test targets
         .testTarget(
