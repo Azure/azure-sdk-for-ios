4493f2fccff0768d6051cef026c5ec25c88a5ddc
Travis Prescott <trpresco@microsoft.com>
2021-03-29 14:45:06 -0700

    Add manifest.yaml and update script to use manifest.

--- eng/scripts/sync_repo.py
+++ eng/scripts/sync_repo.py
@@ -7,10 +7,12 @@ Script to copy code from mono-repo to read-only clone.
 """
 
 import errno
+import json
 import logging
 import os
 import shutil
 import sys
+import yaml
 
 def _log_error_and_quit(msg, out = None, err = None, code = 1):
     """ Log an error message and exit. """
@@ -38,11 +40,21 @@ Copies code from a relative path in the mono-repo to the read-only repo.
 def main(argv):
     usage = f'usage: {__file__} REL_PATH DEST_REPO'
 
-    if len(argv) != 2:
-        _log_error_and_quit(f'usage: {__file__} SOURCE_PATH DEST_REPO')
+    if len(argv) != 1:
+        _log_error_and_quit(f'usage: {__file__} TARGET_SDK')
 
-    source_path = os.path.abspath(os.path.join(ROOT, argv[0]))
-    dest_path = os.path.abspath(os.path.join(ROOT, '..', argv[1]))
+    target = argv[0]
+
+    # Load the manifest where the metadata is registered
+    manifest_path = os.path.abspath(os.path.join(ROOT, 'manifest.yaml'))
+    with open(manifest_path, 'r') as yaml_in:
+        manifest = yaml.load(yaml_in.read(), Loader=yaml.SafeLoader)
+
+    try:
+        source_path = os.path.abspath(os.path.join(ROOT, manifest[target]["source"]))
+        dest_path = os.path.abspath(os.path.join(ROOT, '..', manifest[target]["mirror"]))
+    except IndexError:
+        _log_error_and_quit(f'{target} not found in manifest.yaml')
 
     if not os.path.exists(source_path):
         _log_error_and_quit(f'Source path does not exist: {source_path}')
@@ -51,7 +63,7 @@ def main(argv):
         _log_error_and_quit(f'Destination path does not exist: {dest_path}')
 
     _copy(source_path, dest_path)
-    print(f'Copied {source_path} to {dest_path}...')
+
     sys.exit(0)
 
 
