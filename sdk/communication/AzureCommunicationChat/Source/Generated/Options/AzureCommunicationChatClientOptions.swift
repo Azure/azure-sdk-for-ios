89f08f72a132ff138126e241fc7e53e12e03a8f4
Travis Prescott <trpresco@microsoft.com>
2021-05-24 12:05:54 -0700

    Regenerate.

--- sdk/communication/AzureCommunicationChat/Source/Generated/Options/AzureCommunicationChatClientOptions.swift
+++ sdk/communication/AzureCommunicationChat/Source/Generated/Options/AzureCommunicationChatClientOptions.swift
@@ -14,13 +14,13 @@ import Foundation
 // swiftlint:disable identifier_name
 // swiftlint:disable line_length
 
-/// User-configurable options for the `AzureCommunicationChatClientOptions`.
+/// User-configurable client options.
 public struct AzureCommunicationChatClientOptions: ClientOptions {
-    /// The API version of the  to invoke.
+    /// The API version of the client to invoke.
     public let apiVersion: String
-    /// The `ClientLogger` to be used by this `AzureCommunicationChatClientOptions`.
+    /// The `ClientLogger` to be used by this client.
     public let logger: ClientLogger
-    /// Options for configuring telemetry sent by this `AzureCommunicationChatClientOptions`.
+    /// Options for configuring telemetry sent by this client.
     public let telemetryOptions: TelemetryOptions
     /// Global transport options
     public let transportOptions: TransportOptions
@@ -60,14 +60,14 @@ public struct AzureCommunicationChatClientOptions: ClientOptions {
 
     /// Initialize a `AzureCommunicationChatClientOptions` structure.
     /// - Parameters:
-    ///   - apiVersion: The API version of the  to invoke.
-    ///   - logger: The `ClientLogger` to be used by this `AzureCommunicationChatClientOptions`.
-    ///   - telemetryOptions: Options for configuring telemetry sent by this `AzureCommunicationChatClientOptions`.
+    ///   - apiVersion: The API version of the client to invoke.
+    ///   - logger: The `ClientLogger` to be used by this client.
+    ///   - telemetryOptions: Options for configuring telemetry sent by this client.
     ///   - cancellationToken: A token used to make a best-effort attempt at canceling a request.
     ///   - dispatchQueue: The default dispatch queue on which to call all completion handler. Defaults to `DispatchQueue.main`.
     public init(
         apiVersion: AzureCommunicationChatClientOptions.ApiVersion = .latest,
-        logger: ClientLogger = ClientLoggers.default(tag: "AzureCommunicationChatClientOptionsClient"),
+        logger: ClientLogger = ClientLoggers.default(tag: "AzureCommunicationChat"),
         telemetryOptions: TelemetryOptions = TelemetryOptions(),
         transportOptions: TransportOptions? = nil,
         dispatchQueue: DispatchQueue? = nil
