75d00f60b22d244094bc2f67f03d13f9da08303a
knvsl <10719831+knvsl@users.noreply.github.com>
2021-06-01 14:05:34 -0700

    Setup registrar classes

--- sdk/communication/AzureCommunicationChat/Source/ChatClient.swift
+++ sdk/communication/AzureCommunicationChat/Source/ChatClient.swift
@@ -274,4 +274,12 @@ public class ChatClient {
 
         signalingClient!.off(event: event)
     }
+    
+    public func startPushNotifications() {
+        // Call registrar.setRegistration
+    }
+    
+    public func stopPushNotifications() {
+        // Call registrar.deleteRegistration
+    }
 }
