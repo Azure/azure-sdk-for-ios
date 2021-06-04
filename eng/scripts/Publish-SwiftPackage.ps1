9371b4b0c2de45a580aa05f0575bddd954afb0ee
Mitch Denny <midenn@microsoft.com>
2021-06-05 05:59:11 +1000

    Add SPM publishing to release pipeline (#876)
    * Test checkout of SPM repository.
    
    * Add endpoint field.
    
    * Fix pool name and add directory listing command.
    
    * Fix PowerShell command.
    
    * Look in sources directory.
    
    * Preserve credentials.
    
    * Setup basic copy logic.
    
    * Fix resource repo.
    
    * Add wildcard suffix to work on subfolders.
    
    * Test pushing a tag.
    
    * Differentiate name of branch.
    
    * Fix commit command.
    
    * Add versioning logic.
    
    * Tidy up output.
    
    * Comment tag and cocoapods back in.
    
    * Add repository resources to other pipelines.
    
    * Update storage repository resource and add skip.
    
    * Remove identity swift PM repo resource.
    
    * Pull publishing logic into a script.
    
    * Fix script location for SPM.
    
    * Make SPM publish happen AFTER CocoaPods.
    
    * Prep for merge.

---
+++ eng/scripts/Publish-SwiftPackage.ps1
@@ -0,0 +1,58 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $GitSourcePath,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $GitDestinationPath
+)
+
+$ErrorActionPreference = "Stop"
+
+. (Join-Path $PSScriptRoot/../common/scripts common.ps1)
+
+try {
+
+    Push-Location -Path $GitDestinationPath
+    
+    LogDebug "Searching for *.podspec.json files in: $GitSourcePath"
+    $podspecFiles = Get-ChildItem -Path $GitSourcePath -Filter *.podspec.json
+    LogDebug "Found $($podspecFiles.Count) *.podspec.json files in: $GitSourcePath"
+    
+    if ($podspecFiles.Count -ne 1) {
+        throw "The path $GitSourcePath must contain one, and only one *.podspec.json file."
+    }
+    
+    LogDebug "Parsing $($podspecFiles[0].Name)"
+    $podspec = Get-Content -Raw -Path $podspecFiles[0] | ConvertFrom-Json
+    $name = $podspec.name
+    $version = $podspec.version
+    
+    LogDebug "Publishing: $name@$version"
+    
+    
+    $orphanBranchName = [Guid]::NewGuid()
+    LogDebug "Checking out orphan branch: $orphanBranchName"
+    git checkout --orphan $orphanBranchName
+    git reset --hard
+    
+    LogDebug "Copying package to orphan branch"
+    Copy-Item -Path $GitSourcePath/* -Destination $GitDestinationPath -Recurse
+    
+    LogDebug "Committing files to branch"
+    git add .
+    git commit -m "Releasing $version"
+    
+    LogDebug "Tagging and pushing version: $version"
+    git tag -a $version -m "$version"
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "Failed to tag repository because tag $version already exists!"
+    }
+
+    git push origin $version
+}
+finally {
+    Pop-Location
+}
