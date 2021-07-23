16532894a63c5144fa9ddd1168e215a6adb617e0
Jair Myree <t-jairmyree@microsoft.com>
2021-07-23 16:08:15 -0400

    Removed unused functions and extensions

--- sdk/test/AzureTest/AzureTestTests/AzureTestTests.swift
+++ sdk/test/AzureTest/AzureTestTests/AzureTestTests.swift
@@ -56,7 +56,6 @@ class AzureTestTests: XCTestCase {
     }
 
     func test_scrubbingRequest_removeSubscriptionIDs() throws {
-        chooseRecordedInteraction(index: 0)
         let dirtyURLString = "https://management.azure.com/subscriptions/\(insertedGUID)/resourceGroups/rgname/providers/Microsoft.KeyVault/vaults/myValtZikfikxz?api-version=2019-09-01"
         
         let cleanedURLString = SubscriptionIDFilter().scrubSubscriptionId(from: dirtyURLString)
@@ -81,81 +80,6 @@ class AzureTestTests: XCTestCase {
     }
 
     
-}
-
-
-
-fileprivate extension Array where Element == Any {
-    func dictionary(forIndex: Int) -> Dictionary<String,Any>? {
-        return self[forIndex] as? Dictionary<String, Any>
-    }
-    
-    func array(forIndex: Int) -> Array<Any>? {
-        return self[forIndex] as? Array<Any>
-    }
-    
-    func string(forIndex: Int) -> String? {
-        return self[forIndex] as? String
-    }
-    
-}
-
-fileprivate extension Data {
-    
-    private func topLevelDataDictionary() -> [String:Any] {
-        try! JSONSerialization.jsonObject(with: self, options: []) as! [String:Any]
-    }
-    
-    
-    func responseDataHeaders() -> [String:String?] {
-        let dictionary = topLevelDataDictionary()
-        var stringDictionary = [String:String]()
-        dictionary.forEach {
-            stringDictionary[$0] = ($1 as? CustomStringConvertible)?.description
-        }
-        return stringDictionary
-    }
-    
-    func request(forIndex: Int) -> URLRequest {
-        let dataDictionary = topLevelDataDictionary()
-        var fakeRequest = URLRequest(url: URL(string: dataDictionary.array(forKey: "interactions")?.dictionary(forIndex: forIndex)?.dictionary(forKey: "request")?.string(forKey: "uri"))!)
-        fakeRequest.httpBody = dataDictionary.array(forKey: "interactions")?.dictionary(forIndex: forIndex)?.dictionary(forKey: "request")?.string(forKey: "body")?.data(using: .utf8)
-        return fakeRequest
-    }
-    
-    func response(forIndex: Int) -> URLResponse {
-        let dataDictionary = topLevelDataDictionary()
-        return HTTPURLResponse(url: URL(string: dataDictionary.array(forKey: "interactions")?.dictionary(forIndex: forIndex)?.dictionary(forKey: "request")?.string(forKey: "uri"))!, statusCode: 200, httpVersion: nil, headerFields: dataDictionary.array(forKey: "interactions")?.dictionary(forIndex: forIndex)?.dictionary(forKey: "response")?.dictionary(forKey: "headers")?.stringValues())! as URLResponse
-    }
-    
-    func responseData(forIndex: Int) -> Data {
-        let dataDictionary = topLevelDataDictionary()
-        return (dataDictionary.array(forKey: "interactions")?.dictionary(forIndex: forIndex)?.dictionary(forKey: "response")?.dictionary(forKey: "body")?.description.data(using: .utf8)!)!
-    }
-}
-
-fileprivate extension Dictionary where Key == String, Value == Any {
-    
-     func stringValues() -> [String:String]  {
-         var newDictionary = [String:String]()
-         self.forEach { key, val in
-            newDictionary[key] = (val as! CustomStringConvertible).description
-         }
-         return newDictionary
-     }
-  
-    
-    func dictionary(forKey: Key) -> Dictionary<String,Any>?  {
-        return self[forKey] as? Dictionary<String,Any>
-    }
-    
-    func array(forKey: Key) -> Array<Any>?  {
-        return self[forKey] as? Array<Any>
-    }
-    
-    func string(forKey: Key) -> String? {
-        return self[forKey] as? String
-    }
 }
 
 fileprivate extension String {
