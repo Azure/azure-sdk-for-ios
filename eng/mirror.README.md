f66b5eeb568f81ae33b6ed8fd9de5f7bc262fbf5
Ben Broderick Phillips <bebroder@microsoft.com>
2022-01-27 16:18:40 -0500

    Add release step to publish redirect README to master branch of SwiftPM repository (#1138)
    * Add release step to publish redirect README to master branch of SwiftPM repository
    
    * Add mirror repo template link to ignorelinks.txt

---
+++ eng/mirror.README.md
@@ -0,0 +1,5 @@
+This is a read-only mirror repository for use by the Swift Package Manager.
+
+To view the latest changes, github issues, and pull requests please go to the main umbrella repository [azure-sdk-for-ios](https://github.com/Azure/azure-sdk-for-ios).
+
+To view the documentation and source code for this package you can also select the release version via one of the [git tags](#mirror_repo_url/tags).
---
+++ eng/scripts/Publish-SwiftMirrorRedirectReadme.ps1
@@ -0,0 +1,56 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $GitSourcePath,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $GitDestinationPath,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $UpstreamBranch = "master",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Repository = "azure"
+)
+
+$ErrorActionPreference = "Stop"
+
+. (Join-Path $PSScriptRoot/../common/scripts common.ps1)
+
+Push-Location $GitDestinationPath
+
+git checkout $UpstreamBranch
+
+$repoContents = (Get-ChildItem -Attributes Hidden,!Hidden -Exclude ".git").Name
+# If there is a diff from a single README.md file, clear the repo
+if (Compare-Object $repoContents @("README.md")) {
+    $repoContents | Remove-Item -Recurse -Force
+    LogDebug "Adding commit to clear $UpstreamBranch branch contents"
+    git add -u
+    git commit -m "Clear $UpstreamBranch branch contents"
+}
+
+Copy-Item -Path $GitSourcePath/eng/mirror.README.md -Destination README.md
+$repoUrl = git config --get remote.origin.url
+# Convert ssh remotes to https urls
+if ($repoUrl -imatch ".*($Repository/SwiftPM-.*)") {
+    $repoUrl = "https://github.com/$($matches[1])"
+}
+$readme = cat README.md
+$readme.Replace("#mirror_repo_url", $repoUrl) | Out-File README.md
+
+if (git status --porcelain) {
+    LogDebug "Committing mirror README.md to $UpstreamBranch"
+    git add README.md
+    git commit -m "Add/update mirror README"
+}
+
+if (git diff origin/$UpstreamBranch..HEAD) {
+    LogDebug "Pushing changes to $UpstreamBranch"
+    git push origin $UpstreamBranch
+}
+
+Pop-Location
