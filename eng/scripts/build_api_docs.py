feb56fda2c9683736d961902c88143e13c6b5b4d
Bas Hendri 🐧 <pradiphe@microsoft.com>
2023-12-01 16:40:43 -0800

    supports docs-only Jazzy and adds Vision docs

--- eng/scripts/build_api_docs.py
+++ eng/scripts/build_api_docs.py
@@ -14,6 +14,7 @@ import os
 import subprocess
 import sys
 import yaml
+import time
 
 
 logging.getLogger().setLevel(logging.WARNING)
@@ -36,24 +37,30 @@ def _log_warning(msg):
     end_color = "\033[0m"
     logging.warning(f"{warning_color}{msg}{end_color}")
 
-def _run(command):
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def _run(command, **kwargs):
+    process = subprocess.Popen(command.split() if not kwargs.get("shell", False) else command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
     stdout, stderr = process.communicate()
     return (stdout.decode("utf-8"), stderr.decode("utf-8"))
 
-def _clone_github_repo(path):
+def _clone_github_repo(path, *, ref = None, pod_install = True):
     # clone the repo into the build folder so it can easily be removed
     repo_name = os.path.split(path)[-1]
-    _run(f"git clone {path} build/{repo_name}")
+    branch_arg = f"--branch {ref}" if ref else ""
+    clone_dir = f"build/{repo_name}"
+    if os.path.exists(clone_dir):
+        _run(f"rm -rf {clone_dir}")
 
-    try:
-        # find the podfile and run pod install
-        podfile_path = glob.glob(os.path.abspath(os.path.join(".", "build", repo_name, "**", "Podfile")))[0]
-        podfile_dir = os.path.split(podfile_path)[0]
-        stdout, stderr = _run(f"pod install --project-directory={podfile_dir}")
-        print(stdout)
-    except:
-        _log_warning("Podfile not found. Attempting to generate docs without pod install...")
+    _run(f"git clone {path} build/{repo_name} --depth 1 {branch_arg} --single-branch")
+
+    if pod_install:
+        try:
+            # find the podfile and run pod install
+            podfile_path = glob.glob(os.path.abspath(os.path.join(".", "build", repo_name, "**", "Podfile")))[0]
+            podfile_dir = os.path.split(podfile_path)[0]
+            stdout, stderr = _run(f"pod install --project-directory={podfile_dir}")
+            print(stdout)
+        except:
+            _log_warning("Podfile not found. Attempting to generate docs without pod install...")
 
 if __name__ == "__main__":
     usage = f"usage: python {os.path.split(__file__)[1]} ( NAME ... | all)"
@@ -75,12 +82,26 @@ if __name__ == "__main__":
             with open(path, "r") as config_file:
                 contents = yaml.safe_load(config_file)
                 github_url = contents.get("github_url", None)
-                if not github_url.endswith("azure-sdk-for-ios"):
+                ref = contents.get("ref", None)
+                no_jazzy_mode = contents.get("no_jazzy_mode", None)
+                no_jazzy_mode_pregenerated = True if no_jazzy_mode == "pre-generated" else False
+                if not github_url.endswith("/azure-sdk-for-ios"):
                     print(f"Cloning repo: {github_url}...")
-                    _clone_github_repo(github_url)
+                    _clone_github_repo(path=github_url, ref=ref, pod_install=(not no_jazzy_mode_pregenerated))
 
             print(f"Generating docs for: {path}...")
-            stdout, stderr = _run(f"jazzy --config {path}")
+            if no_jazzy_mode is None:
+                stdout, stderr = _run(f"jazzy --config {path}")
+            elif no_jazzy_mode_pregenerated:
+                time.sleep(5)
+                output = "build/" + contents.get("output", None)
+                input_dir = "build/" + os.path.split(github_url)[-1]
+                print(f"Copying {input_dir} to {output}...")
+                stdout, stderr = _run(f"mkdir -p {output}")
+                stdout, stderr = _run(f"mv {input_dir}/* {output}", shell=True)
+
+            else:
+                logging.error(f"no_jazzy_mode specified but don't know how to handle '{no_jazzy_mode}'.")
             if "RuntimeError" in stderr:
                 logging.error(f"==BEGIN STDERR==.\n{stderr}\n==END STDERR==\nError generating docs.")
             else:
