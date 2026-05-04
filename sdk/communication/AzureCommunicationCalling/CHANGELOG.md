3e9329c6a4c1e72f0eedf28d1ff6e416f6cde6d7
Nancy Pandya <pandyanancy@microsoft.com>
2026-05-04 16:57:36 -0400

    Updating 2.18.3-beta.1 version

--- sdk/communication/AzureCommunicationCalling/CHANGELOG.md
+++ sdk/communication/AzureCommunicationCalling/CHANGELOG.md
@@ -1,5 +1,7 @@
 # Release History
 
+## 2.18.3-beta.1 (2026-05-04) 
+
 ## 2.18.2 (2026-03-10)
 ### arm64 crash in simulator 
 
--- sdk/communication/AzureCommunicationCalling/Package.swift
+++ sdk/communication/AzureCommunicationCalling/Package.swift
@@ -48,13 +48,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AzureCommunicationCalling",
-            url: "https://github.com/Azure/Communication/releases/download/v2.18.2/AzureCommunicationCalling-2.18.2.zip",
-            checksum: "5d910fedb91ad27b2a0c820e7e4a5d6e1c67c27398f49fc440b38d2d56c7661e"
+            url: "https://github.com/Azure/Communication/releases/download/v2.18.3-beta.1/AzureCommunicationCalling-2.18.3-beta.1.zip",
+            checksum: "8a4f3f53cd644d2753c0e2741a454fe4a49c35626f41e3caadb6133b8940927b"
         ),
         .binaryTarget(
             name: "AzureCommunicationCommon",
-            url:"https://github.com/Azure/azure-sdk-for-ios/releases/download/AzureCommunicationCommon_1.3.3/AzureCommunicationCommon_1.3.3.xcframework.zip",
-            checksum: "4694c77d1ef30178197c458195474b78b4e28098c821e0392c420cf5f0762568"
+            url:"https://github.com/Azure/azure-sdk-for-ios/releases/download/AzureCommunicationCommon_1.3.5/AzureCommunicationCommon_1.3.5.xcframework.zip",
+            checksum: "5eda764f33867fb57d45d568a793c57fa6f618bc2f935f86247cfcf4fa117371"
         )
     ]
 )
